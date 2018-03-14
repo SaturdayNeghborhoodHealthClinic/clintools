febc5fb87c0360bea4591a5f00af6c8236ab4fb4
ameller1994 <artur.meller.1994@gmail.com>
2018-03-14 16:16:22 -0500

    Refresh migrations.

--- referral/migrations/0001_initial.py
+++ referral/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pttrack', '0003_auto_20171014_1843'),
+        ('pttrack', '0004_auto_20180213_1616'),
         ('followup', '0001_initial'),
     ]
 
@@ -18,12 +18,12 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('written_datetime', models.DateTimeField(auto_now_add=True)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
+                ('completion_date', models.DateTimeField(null=True, blank=True)),
                 ('due_date', models.DateField(help_text=b'MM/DD/YYYY or YYYY-MM-DD')),
                 ('contact_instructions', models.TextField()),
-                ('completion_date', models.DateField(null=True, blank=True)),
                 ('author', models.ForeignKey(to='pttrack.Provider')),
                 ('author_type', models.ForeignKey(to='pttrack.ProviderType')),
-                ('completion_author', models.ForeignKey(related_name='referral_followup_completed', blank=True, to='pttrack.Provider', null=True)),
+                ('completion_author', models.ForeignKey(related_name='referral_followuprequest_completed', blank=True, to='pttrack.Provider', null=True)),
                 ('patient', models.ForeignKey(to='pttrack.Patient')),
             ],
             options={
@@ -36,15 +36,15 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('written_datetime', models.DateTimeField(auto_now_add=True)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
-                ('has_appointment', models.BooleanField(help_text=b'Did the patient make an appointment?')),
-                ('pt_showed', models.CharField(blank=True, max_length=7, null=True, help_text=b'Did the patient show up to the appointment?', choices=[(b'Yes', b'Yes'), (b'No', b'No'), (b'Not yet', b'Not yet')])),
+                ('has_appointment', models.CharField(max_length=7, verbose_name=b'Did the patient make an appointment?', choices=[(b'Yes', b'Yes'), (b'No', b'No'), (b'Not yet', b'Not yet')])),
+                ('pt_showed', models.CharField(blank=True, max_length=7, null=True, verbose_name=b'Did the patient show up to the appointment?', choices=[(b'Yes', b'Yes'), (b'No', b'No'), (b'Not yet', b'Not yet')])),
                 ('author', models.ForeignKey(to='pttrack.Provider')),
                 ('author_type', models.ForeignKey(to='pttrack.ProviderType')),
-                ('contact_method', models.ForeignKey(to='pttrack.ContactMethod')),
-                ('contact_status', models.ForeignKey(to='followup.ContactResult')),
+                ('contact_method', models.ForeignKey(verbose_name=b'What was the method of contact?', to='pttrack.ContactMethod')),
+                ('contact_status', models.ForeignKey(verbose_name=b'Did you make contact with the patient about this referral?', to='followup.ContactResult')),
                 ('followupRequest', models.ForeignKey(to='referral.FollowupRequest')),
-                ('noapt_reason', models.ForeignKey(blank=True, to='followup.NoAptReason', help_text=b"If the patient didn't make an appointment, why not?", null=True)),
-                ('noshow_reason', models.ForeignKey(blank=True, to='followup.NoShowReason', help_text=b"If the patient didn't go to appointment, why not?", null=True)),
+                ('no_apt_reason', models.ForeignKey(verbose_name=b"If the patient didn't make an appointment, why not?", blank=True, to='followup.NoAptReason', null=True)),
+                ('no_show_reason', models.ForeignKey(verbose_name=b"If the patient didn't go to appointment, why not?", blank=True, to='followup.NoShowReason', null=True)),
                 ('patient', models.ForeignKey(to='pttrack.Patient')),
             ],
             options={
@@ -57,26 +57,43 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('written_datetime', models.DateTimeField(auto_now_add=True)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
-                ('comments', models.TextField()),
+                ('comments', models.TextField(blank=True)),
+                ('kind', models.CharField(max_length=10, choices=[(b'FQHC', b'Primary Care (FQHC)'), (b'SPEC', b'Specialty')])),
                 ('author', models.ForeignKey(to='pttrack.Provider')),
                 ('author_type', models.ForeignKey(to='pttrack.ProviderType')),
-                ('location', models.ForeignKey(to='pttrack.ReferralLocation', blank=True)),
-                ('patient', models.ForeignKey(to='pttrack.Patient')),
-                ('referralType', models.ForeignKey(to='pttrack.ReferralType', blank=True)),
             ],
             options={
                 'abstract': False,
             },
         ),
         migrations.CreateModel(
+            name='ReferralLocation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=300)),
+                ('is_fqhc', models.NullBooleanField()),
+                ('is_specialty', models.NullBooleanField()),
+            ],
+        ),
+        migrations.CreateModel(
             name='ReferralStatus',
             fields=[
-                ('name', models.CharField(max_length=50, serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=50, serialize=False, primary_key=True, choices=[(b'S', b'Successful'), (b'P', b'Pending'), (b'U', b'Unsuccessful')])),
             ],
         ),
         migrations.AddField(
             model_name='referral',
-            name='referral_status',
+            name='location',
+            field=models.ManyToManyField(to='referral.ReferralLocation'),
+        ),
+        migrations.AddField(
+            model_name='referral',
+            name='patient',
+            field=models.ForeignKey(to='pttrack.Patient'),
+        ),
+        migrations.AddField(
+            model_name='referral',
+            name='status',
             field=models.ForeignKey(to='referral.ReferralStatus'),
         ),
         migrations.AddField(
