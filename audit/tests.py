36fead570a20514d72f6f09ae700c6746c8bcac0
Justin R. Porter <justinrporter@gmail.com>
2019-03-03 16:14:58 -0600

    Test middleware remote address parsing.

--- audit/tests.py
+++ audit/tests.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 from pttrack.models import ProviderType
+from pttrack.test_views import build_provider, log_in_provider
 
 from .models import PageviewRecord
 
@@ -33,7 +34,21 @@ class TestAudit(TestCase):
             "GET by None to /pttrack/ at %s" % p.timestamp)
 
     def test_create_on_view(self):
-        self.client.get(reverse('home'))
+
+        provider = log_in_provider(self.client, build_provider(["Attending"]))
+        expected_user = provider.associated_user
+
+        # format is: {X-Forwarded-For: client, proxy1, proxy2}
+        USER_IP = '0.0.0.0'
+        self.client.get(
+            reverse('home'),
+            HTTP_X_FORWARDED_FOR=USER_IP + ',,')
 
         n_records = PageviewRecord.objects.count()
         self.assertEquals(n_records, 1)
+
+        record = PageviewRecord.objects.first()
+        self.assertEquals(record.user, expected_user)
+        self.assertEquals(record.user_ip, USER_IP)
+        self.assertEquals(record.role.short_name, 'Attending')
+        self.assertEquals(record.method, 'GET')
